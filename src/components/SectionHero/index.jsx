ced5f8261143006b68a23dae2c62a84ba7345b29
Gabriel Reis <gabrielreiscunha6@gmail.com>
2024-11-18 22:02:56 -0300

    json added & sectionhero removed

--- src/components/SectionHero/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function SectionHero() {
-  return (
-    <div>
-        <h1>Banner Hero</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </div>
-  )
-}
